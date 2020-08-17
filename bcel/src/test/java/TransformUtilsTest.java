108acec4b374d116046f1682a0169a3c9ea6ada9
kstuglik <krzy_s12@wp.pl>
2020-08-18 00:32:31 +0200

    updated test file for: TransformUtils

--- bcel/src/test/java/TransformUtilsTest.java
+++ bcel/src/test/java/TransformUtilsTest.java
@@ -2,23 +2,21 @@
  * OLD TEST: update is required
  */
 
-import org.apache.bcel.classfile.ClassParser;
-import org.apache.bcel.classfile.Code;
-import org.apache.bcel.classfile.JavaClass;
-import org.apache.bcel.classfile.Method;
+import org.apache.bcel.classfile.*;
+import org.apache.bcel.generic.ClassGen;
 import org.apache.bcel.generic.GETSTATIC;
 import org.apache.bcel.generic.InstructionHandle;
+import org.apache.bcel.generic.MethodGen;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import pl.edu.agh.bcel.transformations.LoopUtils;
-import pl.edu.agh.bcel.transformations.utils.ByteCodeModifier;
-import pl.edu.agh.bcel.transformations.utils.LaunchProperties;
+import pl.edu.agh.bcel.utils.*;
+import pl.edu.agh.bcel.transformations.Variables;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
-import static org.junit.Assert.*;
 
 @FixMethodOrder()
 public class TransformUtilsTest {
@@ -30,39 +28,47 @@ public class TransformUtilsTest {
     }
 
     @Test
-    public void test0() throws Exception {
+    public void test_1() throws Exception {
 //        shouldInjectThreadPoolToClass
         LaunchProperties.CLASS_NAME = "SerialNbody";
-
         LaunchProperties.MODIFICATION_SUFFIX = "_T1";
 
-        ByteCodeModifier bcm = new ByteCodeModifier();
-        bcm.prepareToModify();
+        JavaClass analyzedClass = new ClassParser(LaunchProperties.getPathToIntputFile()).parse();
+        ClassGen cgTarget = new ClassGen(analyzedClass);
 
-        pl.edu.agh.bcel.transformations.TransformUtils.addThreadPoolExecutorService(bcm._modifiedClass);
-        ArrayList<String> fields = bcm.getNameFields();
+        Method transformedMethod = ByteCodeModifier.getSelectedMethod0(cgTarget, LaunchProperties.CLASS_METHOD);
+        MethodGen mg = new MethodGen(transformedMethod, cgTarget.getClassName(), cgTarget.getConstantPool());
 
-        assertTrue(fields.contains(LaunchProperties.NUMBER_OF_THREADS_NAME));
-        assertTrue(fields.contains(LaunchProperties.EXECUTOR_SERVICE_NAME));
+        TransformUtils.addThreadPoolExecutorService(cgTarget);
+        Field[] fields = cgTarget.getFields();
 
-        bcm.saveNewClassFile();
+        System.out.println("GOTO: " + LaunchProperties.getPathToOutputFile());
+        cgTarget.getJavaClass().dump(LaunchProperties.getPathToOutputFile());
+
+        Assert.assertTrue(Variables.checkIfFieldExist(fields, LaunchProperties.NUMBER_OF_THREADS_NAME));
+        Assert.assertTrue(Variables.checkIfFieldExist(fields, LaunchProperties.EXECUTOR_SERVICE_NAME));
+
     }
 
     @Test
-    public void test1() throws IOException {
+    public void test_2() throws IOException {
 //        shouldInjectTaskPoolToClass
         LaunchProperties.CLASS_NAME = "SerialNbody_T1";
-
         LaunchProperties.MODIFICATION_SUFFIX = "_T2";
 
-        ByteCodeModifier bcm = new ByteCodeModifier();
-        bcm.prepareToModify();
-        bcm.extraMe();
+        JavaClass analyzedClass = new ClassParser(LaunchProperties.getPathToIntputFile()).parse();
+        ClassGen cgTarget = new ClassGen(analyzedClass);
 
-        pl.edu.agh.bcel.transformations.TransformUtils.addFieldTaskPool(bcm._modifiedClass, bcm._mg);
-        bcm.saveNewClassFile();
+        Method transformedMethod = ByteCodeModifier.getSelectedMethod0(cgTarget, LaunchProperties.CLASS_METHOD);
+        MethodGen mg = new MethodGen(transformedMethod, cgTarget.getClassName(), cgTarget.getConstantPool());
 
-        LaunchProperties.CLASS_NAME = "SerialNbody_T2";
+        ReadyFields.addFieldTaskPool(cgTarget, mg);
+        ReadyMethods.addMethodToInitTaskPool(cgTarget);
+
+        System.out.println("GOTO: " + LaunchProperties.getPathToOutputFile());
+        cgTarget.getJavaClass().dump(LaunchProperties.getPathToOutputFile());
+
+        LaunchProperties.CLASS_NAME = "SerialNbody_T1_T2";
         JavaClass _analyzedClass = new ClassParser(LaunchProperties.getPathToIntputFile()).parse();
 
         String signature = "";
@@ -70,7 +76,7 @@ public class TransformUtilsTest {
         for (Method method : _analyzedClass.getMethods()) {
             if (method.getName().contains(LaunchProperties.CLASS_METHOD)) {
                 Code code = method.getCode();
-                if (code.toString().contains("Callable")) { // Non-abstract method
+                if (code.toString().contains("Callable")) {
                     String[] table = method.getCode().toString().split("\\s+");
                     for (String s : table) {
                         if (s.contains("Callable")) {
@@ -83,70 +89,80 @@ public class TransformUtilsTest {
             }
         }
 
-        assertEquals("()LArrayList<Callable<Integer>>;", signature);
+        Assert.assertEquals("()LArrayList<Callable<Integer>>;", signature);
     }
 
     @Test
-    public void test2() throws Exception {
+    public void test_3() throws Exception {
 //        shouldCopyLoopToMethod
-        LaunchProperties.CLASS_NAME = "SerialNbody_T2";
-
+        LaunchProperties.CLASS_NAME = "SerialNbody_T1_T2";
         LaunchProperties.MODIFICATION_SUFFIX = "_T3";
 
-        ByteCodeModifier bcm = new ByteCodeModifier();
-        bcm.prepareToModify();
-        bcm.extraMe();
+        JavaClass analyzedClass = new ClassParser(LaunchProperties.getPathToIntputFile()).parse();
+        ClassGen cgTarget = new ClassGen(analyzedClass);
 
-        int before = bcm.getMethodsArray().length;
-        pl.edu.agh.bcel.transformations.TransformUtils.copyLoopToSubTaskMethod(bcm._modifiedClass, bcm._mg, LaunchProperties.LOOP_ITERATOR_NAME);
+        Method transformedMethod = ByteCodeModifier.getSelectedMethod0(cgTarget, LaunchProperties.CLASS_METHOD);
+        MethodGen mg = new MethodGen(transformedMethod, cgTarget.getClassName(), cgTarget.getConstantPool());
 
-        bcm.setMethodsArray();
-        int after = bcm.getMethodsArray().length;
+        int before = cgTarget.getMethods().length;
 
-        assertEquals(before + 1, after);
+//        OLD SOLUTION TO REPLACE OR REBUILD
+        TransformUtils.copyLoopToSubTaskMethod(cgTarget, mg, LaunchProperties.LOOP_ITERATOR_NAME);
 
-        bcm.saveNewClassFile();
+        int after = cgTarget.getMethods().length;
+
+        Assert.assertEquals(before + 1, after);
+
+        System.out.println("GOTO: " + LaunchProperties.getPathToOutputFile());
+        cgTarget.getJavaClass().dump(LaunchProperties.getPathToOutputFile());
     }
 
     //        replace stop condition in loop inside selected(subTask) method with use static field NumThreads
     @Test
-    public void test3() throws Exception {
+    public void test_4() throws Exception {
 //        shouldReplaceLoopIndexWithNumberOfThreads
-        LaunchProperties.CLASS_NAME = "SerialNbody_T3";
-
+        LaunchProperties.CLASS_NAME = "SerialNbody_T1_T2_T3";
         LaunchProperties.MODIFICATION_SUFFIX = "_T4";
 
-        ByteCodeModifier bcm = new ByteCodeModifier();
-        bcm.prepareToModify();
-        bcm.extraMe();
+        JavaClass analyzedClass = new ClassParser(LaunchProperties.getPathToIntputFile()).parse();
+        ClassGen cgTarget = new ClassGen(analyzedClass);
 
-        pl.edu.agh.bcel.transformations.TransformUtils.changeLoopLimitToNumberOfThreads(bcm._modifiedClass, bcm._mg);
+        Method transformedMethod = ByteCodeModifier.getSelectedMethod0(cgTarget, LaunchProperties.CLASS_METHOD);
+        MethodGen mg = new MethodGen(transformedMethod, cgTarget.getClassName(), cgTarget.getConstantPool());
 
-        InstructionHandle[] forLoop = LoopUtils.getForLoop(bcm._mg);
+        TransformUtils.changeLoopLimitToNumberOfThreads(cgTarget, mg);
 
-        assertTrue(forLoop[3].getInstruction() instanceof GETSTATIC);
+        System.out.println("GOTO: " + LaunchProperties.getPathToOutputFile());
+        cgTarget.getJavaClass().dump(LaunchProperties.getPathToOutputFile());
 
-        bcm.saveNewClassFile();
+        InstructionHandle[] forLoop = LoopUtils.getForLoop(mg);
+//        WHY INDEX: 3?
+        Assert.assertTrue(forLoop[3].getInstruction() instanceof GETSTATIC);
+
     }
 
     @Test
-    public void test4() {
+    public void test_5() throws IOException {
 //        shouldThrowWhenNoNumberOfThreadsFieldIsPresent
-        LaunchProperties.CLASS_NAME = "SerialNbody_T4";
+        LaunchProperties.CLASS_NAME = "SerialNbody";
+        LaunchProperties.CLASS_METHOD = "main";
         LaunchProperties.MODIFICATION_SUFFIX = "_T5";
 
-        ByteCodeModifier bcm = new ByteCodeModifier();
+        JavaClass analyzedClass = new ClassParser(LaunchProperties.getPathToIntputFile()).parse();
+        ClassGen cgTarget = new ClassGen(analyzedClass);
+
+        Method transformedMethod = ByteCodeModifier.getSelectedMethod0(cgTarget, LaunchProperties.CLASS_METHOD);
+        MethodGen mg = new MethodGen(transformedMethod, cgTarget.getClassName(), cgTarget.getConstantPool());
+
 
         try {
-            bcm.prepareToModify();
-            bcm.extraMe();
-            pl.edu.agh.bcel.transformations.TransformUtils.changeLoopLimitToNumberOfThreads(bcm._modifiedClass, bcm._mg);
-            bcm.saveNewClassFile();
+            TransformUtils.changeLoopLimitToNumberOfThreads(cgTarget, mg);
+            System.out.println("GOTO: " + LaunchProperties.getPathToOutputFile());
+            cgTarget.getJavaClass().dump(LaunchProperties.getPathToOutputFile());
         } catch (Exception e) {
             System.out.println("************************************* DONE ***********************************");
-            System.out.println("IllegalStateException: " + (e.getClass() == IllegalStateException.class));
-
-            assertSame(e.getClass(), IllegalStateException.class);
+            System.out.println("IllegalArgumentException: " + (e.getClass() == IllegalArgumentException.class));
+            Assert.assertSame(IllegalArgumentException.class, e.getClass());
         }
 
     }
